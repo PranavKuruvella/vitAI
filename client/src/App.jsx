d6d0a16d906dc377e8432aa2d4bd34b65eb4bf63
pranavkuruvella <pranavkuruvella@gmail.com>
2026-01-11 15:59:33 +0530

    connecting client nd server , feature added for login,signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Layout from './pages/Layout.jsx'
@@ -6,21 +6,50 @@ import Dashboard from './pages/Dashboard.jsx'
 import ResumeBuilder from './pages/ResumeBuilder.jsx'
 import Preview from './pages/Preview.jsx'
 import Login from './pages/Login.jsx'
+import { useDispatch } from 'react-redux'
+import api from './configs/api.js'
+import { login, setLoading } from './app/features/authSlice.js'
 
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+
+  const getUserData = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      if (token) {
+        const { data } = await api.get('/api/users/data', { headers: { Authorization: token } })
+        if (data.user) {
+          dispatch(login({ token, user: data.user }))
+        }
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    } catch (error) {
+      dispatch(setLoading(false));
+      console.log(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <>
-    <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='app' element={<Layout/>}> 
-    {/* app ani start chesithe eekadaki po */}
-      <Route index element={<Dashboard/>}/>
-      <Route path='builder/:resumeId' element={<ResumeBuilder/>}/>
-    </Route>
-    <Route path='view/:resumeId' element={<Preview/>}/>
-    <Route path='login' element={<Login/>}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='app' element={<Layout />}>
+          {/* app ani start chesithe eekadaki po */}
+          <Route index element={<Dashboard />} />
+          <Route path='builder/:resumeId' element={<ResumeBuilder />} />
+        </Route>
+        <Route path='view/:resumeId' element={<Preview />} />
+
+      </Routes>
     </>
   )
 }
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Outlet, Link } from 'react-router-dom'
 
 import Navbar from '../components/NavBar.jsx'
+import Loader from '../components/Loader.jsx'
+import Login from './Login.jsx'
 
 const Layout = () => {
+
+  const { user, loading } = useSelector((state) => state.auth);
+
+  if (loading) {
+    return <Loader />
+  }
+
+
   return (
     <div>
-      <div className='min-h-screen bg-gray-50'>
-        <Navbar />
-        <Outlet />
-      </div>
+      {
+        user ? (
+          <div className='min-h-screen bg-gray-50'>
+            <Navbar />
+            <Outlet />
+          </div>
+        ) : <Login />
+      }
     </div>
   )
 }
